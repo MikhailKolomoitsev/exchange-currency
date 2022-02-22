abe2efc3a882c13770838d8f21329ab222140ca0
mihon.testosterone <mihon.testosterone@gmail.com>
2022-02-22 21:13:41 +0000

    feature with dark/light mode

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import './App.css';
+import './styles/App.css';
 // import Converter from './components/Converter';
 import Header from './components/Header';
 import { BrowserRouter } from "react-router-dom";
 import { useState, useEffect } from 'react'
 
 function App() {
-  const [dark, setdark] = useState(false)
-  function changeTheme() {
-   setdark(prevValue=>prevValue) 
-  }
-  useEffect(() => {
-    console.log('use effect works');
-  }, [dark])
+
   
   return (
-    <div className="App" style={{ backgroundColor: dark ?'#000':'#fff'}}>
+    <div className="App">
       <BrowserRouter>
-        <Header toggleTheme={changeTheme} value={dark}></Header>
+        <Header ></Header>
         <div style={{height:300}}></div>
       </BrowserRouter>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,25 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { NavLink } from 'react-bootstrap';
-import ToggleButton from 'react-toggle-button'
-import { fetchData } from '../http/currencyAPI';
+import ThemeButton from './DarkMode';
 
-const Header = ({toggleTheme, value}) => {
-    const [theme, setTheme] = useState(value)
+const Header = () => {
 
     return (
         <div style={{
-            display: 'flex',
-            alignItems: 'baseline',
-            backgroundColor: theme ? 'black' : '#a5a5a5',
-            border: theme ? 'thick solid black' : 'thick solid #a5a5a5'
+            backgroundColor:'#a5a5a5',
+            // border: theme ? 'thick solid black' : 'thick solid #a5a5a5'
         }} className='d-flex justify-content-between p-2 m-0'>
             <div className='d-flex'>
                 <NavLink to={'/'} style={{ color: 'white' }}>All Currencies</NavLink>
                 <NavLink to={'/my-currencies'} style={{ color: 'white' }}>My Currencies</NavLink>
             </div>
-            <ToggleButton
-                animateThumbStyleHover={(n => {
-                    return {
-                        boxShadow: `0 0 ${2 + 4 * n}px rgba(0,0,0,.16),0 ${2 + 3 * n}px ${4 + 8 * n}px rgba(0,0,0,.32)`,
-                    }
-                })}
-                inactiveLabel={'Light'}
-                activeLabel={'Dark'}
-                value={theme}
-                onToggle={() => {
-                    setTheme(!theme)
-                }} />
+            <ThemeButton />
         </div>
 
     )
 }
 
 export default Header
+
+
